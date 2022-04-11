dd1b508b96194d657f9b6ddf015a74090b484378
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-04-11 16:46:54 +0600

    sign in & sign out linking auth provider credentials to an existing user account

--- src/steps.js
+++ src/steps.js
@@ -4,8 +4,10 @@
 * 2. npm install firebase
 * 3. Register web app in firebase
 * 4. Copy firebase init with config from firebase project settings into a file firebase.init.js
-* 5. export default app
+* 5. export default app from firebase.init.js
 * 6. import app firebase init.js into your app.js
 * 7. import getAuth from firebase/auth and create auth = getAuth(app)
-* 8. turn on google authentication
+* 8. turn on google authentication (firebase > authentication > enable google sign in)
+* 9. create google provider
+* 10. 
 */ 
